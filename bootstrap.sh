d16809a2bdf712a220ec0c42227a7225a229fa85
Stian Thaulow <stian@thaulow.me>
2021-01-21 19:55:56 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install zsh if not installed
+if [ $(dpkg-query -W -f='${Status}' zsh 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then	
+  apt-get install -y zsh;
+fi
+
+if [ ! -d ~/.oh-my-zsh ];
+then   
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  # Link .zshrc
+  ln -s ~/dotfiles/.zshrc ~/.zshrc
+fi
+
