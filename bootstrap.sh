9ecc3b1715fb95edd613b7cab92a37320e81d313
Stian Thaulow <stian@thaulow.me>
2021-01-21 21:00:56 +0100

    Add sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Install zsh if not installed
-if [ $(dpkg-query -W -f='${Status}' zsh 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then	
-  apt-get install -y zsh;
+if [ $(dpkg-query -W -f='${Status}' zsh 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    sudo apt-get install -y zsh
 fi
 
-if [ ! -d ~/.oh-my-zsh ];
-then   
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-  # Link .zshrc
-  ln -s ~/dotfiles/.zshrc ~/.zshrc
+    # Link .zshrc
+    ln -s ~/dotfiles/.zshrc ~/.zshrc
 fi
-
