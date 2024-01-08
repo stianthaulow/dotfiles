16372e9fcd30a21703c267bfd95fd2e05bea9db1
Stian Thaulow <stian.thaulow@malling.no>
2024-01-08 07:22:26 +0100

    Add PowerShell module installation script for Windows

--- .chezmoiscripts/Windows/run_once_05-powershell-modules.ps1
+++ .chezmoiscripts/Windows/run_once_05-powershell-modules.ps1
@@ -1,3 +1,17 @@
 Write-Host "Installing Powershell modules..."
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (!$isAdmin) {
+  $script = $MyInvocation.MyCommand.Definition
+  Start-Process pwsh -ArgumentList "-File `"$script`"" -Verb RunAs -Wait  
+  exit
+}
+
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
 Install-Module DockerCompletion -Scope CurrentUser
 Install-Module posh-git -Scope CurrentUser
+
+Write-Host "Powershell modules installed." -ForegroundColor Green
