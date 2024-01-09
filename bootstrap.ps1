380f216d9a18da764852961b63d8ffaaa13c488e
Stian Thaulow <stian.thaulow@malling.no>
2024-01-09 10:44:27 +0100

    Add winget installation prompt and setup message

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,1 +1,5 @@
 Start-Process "ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1"
+Write-Host "Press any key to continue after installing winget"
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+
+Write-Host "Setting up.."
