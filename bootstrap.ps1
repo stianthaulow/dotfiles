416ef90d8eb52698e1a085194467dd9e97533d2d
Stian Thaulow <stian.thaulow@malling.no>
2024-01-15 15:26:43 +0100

    Update winget installation process

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -51,7 +51,7 @@ catch {
 }
 
 if (-not $currentWingetVersion -or $currentWingetVersion -lt $latestVersion) {
-  Write-Host "Installing winget..."
+  Write-Host "Downloading lastet winget..."
   $wingetPackageName = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
   $packageUrl = $response.assets | Where-Object { $_.Name -eq $wingetPackageName } | Select-Object -ExpandProperty browser_download_url
 
@@ -61,6 +61,7 @@ if (-not $currentWingetVersion -or $currentWingetVersion -lt $latestVersion) {
   $ProgressPreference = 'SilentlyContinue'  
   Invoke-WebRequest -Uri $packageUrl -OutFile $packagePath
   $ProgressPreference = 'Continue'
+  Write-Host "Installing winget..."
   Add-AppxPackage -Path $packagePath
   Remove-item $tempFolderPath -Recurse -Force -ErrorAction SilentlyContinue
 }
