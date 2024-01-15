fce9797aa89296921fe8bce266e42c7221d12025
Stian Thaulow <stian@thaulow.me>
2024-01-15 23:24:58 +0100

    Update bootstrap.ps1 to download and install the latest version of winget and Microsoft.UI.Xaml

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -32,6 +32,7 @@ if (!$isAdmin) {
   Write-Debug "Done"
   exit
 }
+
 $wingetApiUrl = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 $response = Invoke-RestMethod -Uri $wingetApiUrl
 
@@ -51,17 +52,21 @@ catch {
 }
 
 if (-not $currentWingetVersion -or $currentWingetVersion -lt $latestVersion) {
-  Write-Host "Downloading lastet winget..."
+  Write-Host "Downloading latest winget..."
   $wingetPackageName = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
   $packageUrl = $response.assets | Where-Object { $_.Name -eq $wingetPackageName } | Select-Object -ExpandProperty browser_download_url
+  $xamlUIUrl = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx"
 
   $tempFolderPath = Join-Path -Path $env:Temp -ChildPath "Winget"
   New-Item -ItemType Directory -Path $tempFolderPath | Out-Null
   $packagePath = Join-Path -Path $tempFolderPath -ChildPath $(Split-Path -Leaf $packageUrl)
-  $ProgressPreference = 'SilentlyContinue'  
+  $xamlUiPath = Join-Path -Path $tempFolderPath -ChildPath $(Split-Path -Leaf $xamlUIUrl)
+  $ProgressPreference = 'SilentlyContinue'
+  Invoke-WebRequest -Uri $xamlUIUrl -OutFile $xamlUiPath
   Invoke-WebRequest -Uri $packageUrl -OutFile $packagePath
   $ProgressPreference = 'Continue'
   Write-Host "Installing winget..."
+  Add-AppxPackage -Path $xamlUiPath
   Add-AppxPackage -Path $packagePath
   Remove-item $tempFolderPath -Recurse -Force -ErrorAction SilentlyContinue
 }
