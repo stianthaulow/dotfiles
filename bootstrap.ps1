90238ea623ecf60aa93920bef91abba3869b18ff
Stian Thaulow <stian@thaulow.me>
2024-01-16 15:05:54 +0100

    Add XAML UI and VCLibs packages to bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -185,14 +185,22 @@ if (-not $currentWingetVersion -or $currentWingetVersion -lt $latestVersion) {
   Write-Host "Downloading latest winget..."
   $wingetPackageName = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
   $packageUrl = $response.assets | Where-Object { $_.Name -eq $wingetPackageName } | Select-Object -ExpandProperty browser_download_url
+  $xamlUiUrl = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx"
+  $vclibsUrl = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
 
   $tempFolderPath = Join-Path -Path $env:Temp -ChildPath "Winget"
   New-Item -ItemType Directory -Path $tempFolderPath | Out-Null
   $packagePath = Join-Path -Path $tempFolderPath -ChildPath $(Split-Path -Leaf $packageUrl)
+  $xamlUiPath = Join-Path -Path $tempFolderPath -ChildPath $(Split-Path -Leaf $xamlUiUrl)
+  $vclibsPath = Join-Path -Path $tempFolderPath -ChildPath $(Split-Path -Leaf $vclibsUrl)
   $ProgressPreference = 'SilentlyContinue'  
   Invoke-WebRequest -Uri $packageUrl -OutFile $packagePath
+  Invoke-WebRequest -Uri $xamlUiUrl -OutFile $xamlUiPath
+  Invoke-WebRequest -Uri $vclibsUrl -OutFile $vclibsPath
   $ProgressPreference = 'Continue'
   Write-Host "Installing winget..."
+  Add-AppxPackage -Path $vclibsPath
+  Add-AppxPackage -Path $xamlUiPath
   Add-AppxPackage -Path $packagePath
   Remove-item $tempFolderPath -Recurse -Force -ErrorAction SilentlyContinue
 }
