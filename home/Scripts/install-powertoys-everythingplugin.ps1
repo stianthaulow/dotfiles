dd2f00d3ac756f99ddef59f3829c5cd7ab89b8ba
Stian Thaulow <stian@thaulow.me>
2024-01-20 20:40:05 +0100

    Add debug statements to install-powertoys-everythingplugin.ps1

--- home/Scripts/install-powertoys-everythingplugin.ps1
+++ home/Scripts/install-powertoys-everythingplugin.ps1
@@ -5,12 +5,15 @@ if (Test-Path -Path $everythingPluginPath) {
   $pluginManifestPath = Join-Path -Path $everythingPluginPath -ChildPath "plugin.json"
   $pluginManifest = Get-Content -Path $pluginManifestPath -Raw | ConvertFrom-Json
   $installedVersion = $pluginManifest.Version
+  Write-Debug "Installed version: $installedVersion"
 }
 
 $repoOwner = 'lin-ycv'
 $repoName = 'EverythingPowerToys'
 $apiUrl = "https://api.github.com/repos/$repoOwner/$repoName/releases/latest"
 $response = Invoke-RestMethod -Uri $apiUrl
+
+Write-Debug "Response: $response"
 
 $latestTag = ($response.tag_name).TrimStart("v")
 
@@ -20,6 +23,7 @@ if ($latestTag -eq $installedVersion) {
 }
 
 if ($response.assets.Count -gt 0) {
+  Write-Debug "Response assets: $($response.assets)"
   $downloadUrl = $response.assets[0].browser_download_url
   $tempFolderPath = Join-Path -Path $env:Temp -ChildPath "Everything"
   Remove-item $tempFolderPath -Recurse -Force -ErrorAction SilentlyContinue
