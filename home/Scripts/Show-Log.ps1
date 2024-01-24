c9be841b6780c8821423981e5e29b8139710c83f
Stian Thaulow <stian.thaulow@malling.no>
2024-01-24 13:18:42 +0100

    Add logging functionality to Show-Log.ps1 script

--- home/Scripts/Show-Log.ps1
+++ home/Scripts/Show-Log.ps1
@@ -1,2 +1,6 @@
+Start-Transcript -Path "$env:USERPROFILE\dot_transcript.log" -Append
 $logPath = "$env:USERPROFILE\dot.log"
+if (-not (Test-Path $logPath)) {
+  New-Item -Path $logPath -ItemType File | Out-Null
+}
 Get-Content -Path $logPath -Wait
