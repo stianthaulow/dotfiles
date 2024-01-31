d4acb55b98af28e527c054fdb476fe3bba67e00a
Stian Thaulow <stian.thaulow@malling.no>
2024-01-31 11:55:11 +0100

    Add chezmoi initialization and apply changes to bootstrap.sh Add gtk-3.0 dark theme preference to settings.ini

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 DEBIAN_FRONTEND=noninteractive apt-get -yq install git
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply stianthaulow
+sh -c "$(curl -fsLS get.chezmoi.io)"
+~/bin/chezmoi init --apply stianthaulow
