5af046c2d65c4ca4c4cab55a57906a00faffdf11
Stian Thaulow <stian.thaulow@malling.no>
2024-01-31 11:09:30 +0100

    Add script to set dark color scheme and wallpaper

---
+++ home/.chezmoiscripts/Debian/run_once_after_gsettings.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.background picture-uri-dark file:///$HOME/Theme/Wallpaper/wallpaper.jpg
