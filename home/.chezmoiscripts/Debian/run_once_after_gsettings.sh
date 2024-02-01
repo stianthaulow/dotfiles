1d90541dca4f858cbbce2c07b10177652d1f6cc1
Stian Thaulow <stian.thaulow@malling.no>
2024-02-01 12:11:22 +0100

    Remove unnecessary scripts and add setup script

--- home/.chezmoiscripts/Debian/run_once_after_gsettings.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.background picture-uri-dark file:///$HOME/Theme/Wallpaper/wallpaper.jpg
---
+++ home/.chezmoiscripts/Debian/run_once_after_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Installing packages..."
+
+DEBIAN_FRONTEND=noninteractive apt-get -yq install git
+DEBIAN_FRONTEND=noninteractive apt-get -yq install console-data
+
+loadkeys no
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.background picture-uri-dark file:///$HOME/Theme/Wallpaper/wallpaper.jpg
