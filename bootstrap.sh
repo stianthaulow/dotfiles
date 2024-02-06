53edf363fefb8f40e6765bdc51cbc2e392a64900
Stian Thaulow <stian.thaulow@malling.no>
2024-02-06 15:43:50 +0100

    Update bootstrap.sh and readme.md

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-DEBIAN_FRONTEND=noninteractive apt-get -yq install git
-sh -c "$(curl -fsLS get.chezmoi.io)"
+if [ -f "/etc/arch-release" ]; then
+    pacman -S --noconfirm git chezmoi
+    
+else
+    DEBIAN_FRONTEND=noninteractive apt-get -yq install git
+    sh -c "$(curl -fsLS get.chezmoi.io)"
+fi
+
 ~/bin/chezmoi init --apply stianthaulow
--- readme.md
+++ readme.md
@@ -7,9 +7,7 @@ iwr "https://stian.link/init" | iex
 ```sh
 # Debian
 sudo apt-get -yq install curl
-
-# Arch
-pacman -S curl
+#
 
 sudo sh -c "$(curl -fsLS https://stian.link/linux)"
 ```
