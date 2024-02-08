6dcef5a919ee353a2b2b43478a6fefe5bcbbf041
Stian Thaulow <stian@thaulow.me>
2024-02-08 07:32:47 +0100

    Update bootstrap.sh to use sudo for pacman command

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f "/etc/arch-release" ]; then
-    pacman -S --noconfirm git chezmoi
-    exec chezmoi init --apply stianthaulow
+    sudo pacman -S --noconfirm git chezmoi
+    chezmoi init --apply stianthaulow
 else
     DEBIAN_FRONTEND=noninteractive apt-get -yq install git
     sh -c "$(curl -fsLS get.chezmoi.io)"
