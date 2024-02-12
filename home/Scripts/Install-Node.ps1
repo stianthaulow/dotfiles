88a88df29bc1c9b98eae3e80745dd09c025cc1a3
Stian Thaulow <stian@thaulow.me>
2024-02-12 13:15:20 +0100

    Refresh path before checking for fnm in Node install

--- home/Scripts/Install-Node.ps1
+++ home/Scripts/Install-Node.ps1
@@ -1,5 +1,7 @@
 . (Join-Path $PSScriptRoot "Util.ps1")
 
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
 if (Get-Command fnm -ErrorAction SilentlyContinue) {
   Write-Log "Installing node LTS"
   fnm install --lts
