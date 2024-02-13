3df304dcf9f2c8e707e957655505f6c075a6296e
Stian Thaulow <stian@thaulow.me>
2024-02-13 15:22:07 +0100

    Add conditional check for .local/bin directory in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
 if [ -f "/etc/arch-release" ]; then
     sudo pacman -S --noconfirm git chezmoi
-    chezmoi init --apply stianthaulow
 else
     DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install git
     sh -c "$(curl -fsLS get.chezmoi.io/lb)"
-    ~/.local/bin/chezmoi init --apply stianthaulow
 fi
+
+chezmoi init --apply stianthaulow
