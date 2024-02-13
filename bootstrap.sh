504c6b274dda02b8c7fb917eba310cd79926aff6
Stian Thaulow <stian@thaulow.me>
2024-02-13 13:51:42 +0100

    Update bootstrap.sh and dot_zshrc***

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ if [ -f "/etc/arch-release" ]; then
     chezmoi init --apply stianthaulow
 else
     DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install git
-    sh -c "$(curl -fsLS get.chezmoi.io)"
-    ~/bin/chezmoi init --apply stianthaulow
+    sh -c "$(curl -fsLS get.chezmoi.io/lb)"
+    ~/.local/bin/chezmoi init --apply stianthaulow
 fi
