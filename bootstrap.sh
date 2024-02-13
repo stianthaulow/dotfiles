da774238e93a1a010dffc338025af20045f8495c
Stian Thaulow <stian@thaulow.me>
2024-02-13 08:54:55 +0100

    Add sudo to apt-get command in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ if [ -f "/etc/arch-release" ]; then
     sudo pacman -S --noconfirm git chezmoi
     chezmoi init --apply stianthaulow
 else
-    DEBIAN_FRONTEND=noninteractive apt-get -yq install git
+    DEBIAN_FRONTEND=noninteractive sudo apt-get -yq install git
     sh -c "$(curl -fsLS get.chezmoi.io)"
     ~/bin/chezmoi init --apply stianthaulow
 fi
