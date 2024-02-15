6c6d026ae0c75472da4466435167b72c03eea56d
Stian Thaulow <stian@thaulow.me>
2024-02-15 14:07:03 +0100

    Move nvim.appimage to /usr/local/bin/nvim

--- home/.chezmoiscripts/Debian/run_once_after_setup.sh
+++ home/.chezmoiscripts/Debian/run_once_after_setup.sh
@@ -26,7 +26,7 @@ fi
 if ! command -v nvim &> /dev/null; then
     curl -LOs https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
     chmod u+x nvim.appimage
-    mv nvim.appimage /usr/local/bin/nvim
+    sudo mv nvim.appimage /usr/local/bin/nvim
 fi
 
 if command -v gnome-shell >/dev/null 2>&1; then
