a317ca10a7af88dec864b40198c159508b712812
Stian Thaulow <stian@thaulow.me>
2024-02-15 12:58:33 +0100

    Add eza installation script for Debian

--- home/.chezmoiscripts/Debian/run_once_after_setup.sh
+++ home/.chezmoiscripts/Debian/run_once_after_setup.sh
@@ -30,7 +30,7 @@ if ! command -v nvim &> /dev/null; then
 fi
 
 # Install eza if not installed
-if ! command -v eza &> /dev/null; then echo "eza not found"; fi
+if ! command -v eza &> /dev/null;
     curl https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
     echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
     sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
