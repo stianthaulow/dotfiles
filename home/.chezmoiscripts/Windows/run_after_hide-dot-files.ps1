4a7124549009b3545b89d2ffeb13b19b19162f0c
Stian Thaulow <stian@thaulow.me>
2024-04-04 10:40:57 +0200

    Hide dot files except for .gitconfig

--- home/.chezmoiscripts/Windows/run_after_hide-dot-files.ps1
+++ home/.chezmoiscripts/Windows/run_after_hide-dot-files.ps1
@@ -5,6 +5,9 @@ if ($env:DOT_DEBUG -eq "1") {
 $homeChildrenDotFiles = Get-ChildItem -Path $env:USERPROFILE | Where-Object { $_.Name -like ".*" }
 
 foreach ($path in $homeChildrenDotFiles) {
+  if ($path.Name -eq ".gitconfig") {
+    continue
+  }
   $path = $path.FullName
   Write-Debug "Hiding $path"
   Set-ItemProperty -Path $path -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
