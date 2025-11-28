fbca1febc5ee242067fdae05fba22b645aaac55b
Stian Thaulow <stian.thaulow@malling.no>
2025-11-28 09:52:04 +0100

    yazi pwsh

--- home/Powershell/Microsoft.PowerShell_profile.ps1
+++ home/Powershell/Microsoft.PowerShell_profile.ps1
@@ -54,6 +54,16 @@ function hot {
   code $env:USERPROFILE\Autohotkey
 }
 
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
 function rmrf {
   Param(
     [Parameter(Mandatory = $true)]
